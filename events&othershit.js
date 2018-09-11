4c214c6b2c1a4c3f7cf336bf2d0b2b6982a90cac
DickHurtz <39329057+DickHurtz@users.noreply.github.com>
2018-09-11 14:03:15 +0200

    Add files via upload

---
+++ events&othershit.js
@@ -0,0 +1,23 @@
+function Events(){
+	
+}
+function otherShit(){
+	
+}
+var xyuTa = [
+	"ETO RPG BLEAD!"
+];
+var Prosby = [
+	"1. Hej!",
+	"2. Allo blyad",
+	"3. TbI nuDop EnTa"
+];
+var Pizdezh = [
+	"uDu Haxyu",
+	"cheeki breeki",
+	"cbe6ucb",
+	"A Mo}|{eT TbI nuDop?"
+];
+var Otvety = [
+	
+];
